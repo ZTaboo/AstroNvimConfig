a9ad172beed4ca22a3de188c17c639aee701405f
zero <166997982@qq.com>
2023-11-07 13:47:29 +0800

    update

--- plugins/user.lua
+++ plugins/user.lua
@@ -10,43 +10,36 @@ return {
   --   end,
   -- },
   {
-      "goolord/alpha-nvim",
-      opts = function(_, opts) -- override the options using lazy.nvim
-        opts.section.header.val = { -- change the header section value
-        }
-      end,
-    },
-  {
-  "okuuva/auto-save.nvim",
-  cmd = "ASToggle", -- optional for lazy loading on command
-  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
-  opts = {
-    -- your config goes here
-    -- or just leave it empty :)
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
     	enabled = true,
     	execution_message = {
-    enabled = true,
-    message = function() -- message to print on save
-      return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-    end,
-    dim = 0.18, -- dim the color of `message`
-    cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-  },
-  trigger_events = { -- See :h events
-    immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
-    defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
-    cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
-  },
-  -- function that takes the buffer handle and determines whether to save the current buffer or not
-  -- return true: if buffer is ok to be saved
-  -- return false: if it's not ok to be saved
-  -- if set to `nil` then no specific condition is applied
-  condition = nil,
-  write_all_buffers = false, -- write all buffers when the current one meets `condition`
-  noautocmd = false, -- do not execute autocmds when saving
-  debounce_delay = 1000, -- delay after which a pending save is executed
- -- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
-  debug = false,
+        enabled = true,
+        message = function() -- message to print on save
+          return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+        end,
+        dim = 0.18, -- dim the color of `message`
+        cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+      },
+      trigger_events = { -- See :h events
+        immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+        defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+        cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+      },
+      -- function that takes the buffer handle and determines whether to save the current buffer or not
+      -- return true: if buffer is ok to be saved
+      -- return false: if it's not ok to be saved
+      -- if set to `nil` then no specific condition is applied
+      condition = nil,
+      write_all_buffers = false, -- write all buffers when the current one meets `condition`
+      noautocmd = false, -- do not execute autocmds when saving
+      debounce_delay = 1000, -- delay after which a pending save is executed
+      -- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
+      debug = false,
     },
   },
   {
@@ -54,13 +47,41 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        require 'nordic' .load()
+      require 'nordic' .load()
     end
-},
+  },
   {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-}
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "ggandor/leap.nvim",
+    enabled = true,
+    keys = {
+      { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+      { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    },
+    config = function(_, opts)
+      local leap = require("leap")
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ "x", "o" }, "x")
+      vim.keymap.del({ "x", "o" }, "X")
+    end,
+  },
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+    config = function ()
+      vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+    end
+  }
 }
